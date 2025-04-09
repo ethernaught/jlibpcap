72b0da0393213babbfcd8ecd4bbc9b04af776bd0
DrBrad <brad@bradeagle.com>
2025-04-09 14:01:20 -0600

    IPv4Layer

--- src/net/ethernaught/packet/layers/ip/IPv4Layer.java
+++ src/net/ethernaught/packet/layers/ip/IPv4Layer.java
@@ -6,6 +6,7 @@ import net.ethernaught.packet.layers.ip.inter.IPVersions;
 
 import java.lang.reflect.MalformedParametersException;
 import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
 public class IPv4Layer implements Layer {
@@ -13,8 +14,8 @@ public class IPv4Layer implements Layer {
     public static final int IPV4_HEADER_LEN = 20;
 
     private IPVersions version;
-    private byte ihl, tos, flags, ttl;
-    private int totalLength, identification, fragmentOffset, checksum;
+    private byte ihl = 4, tos = 5, flags, ttl = 64;
+    private int totalLength = IPV4_HEADER_LEN, identification, fragmentOffset, checksum;
     private IPProtocols protocol;
     private Inet4Address sourceAddress, destinationAddress;
     private Layer data;
@@ -28,6 +29,24 @@ public class IPv4Layer implements Layer {
     public IPv4Layer(ByteBuffer buf){
         if(buf.remaining() < IPV4_HEADER_LEN){
             throw new MalformedParametersException("Buffer is not long enough to be ethernet frame.");
+        }
+
+        version = IPVersions.fromCode((byte) (buf.get(0) >> 4));
+        ihl = (byte) (buf.get(0) & 0x0F);
+        tos = buf.get(1);
+        totalLength = ((buf.get(2) & 0xFF) << 8) | (buf.get(3) & 0xFF);
+        identification = ((buf.get(4) & 0xFF) << 8) | (buf.get(5) & 0xFF);
+        flags = (byte) (buf.get(6) >> 5);
+        fragmentOffset = ((buf.get(6) & 0x1F) << 8) | (buf.get(7) & 0xFF);
+        ttl = buf.get(8);
+        checksum = ((buf.get(10) & 0xFF) << 8) | (buf.get(11) & 0xFF);
+
+        try{
+            sourceAddress = (Inet4Address) Inet4Address.getByAddress(new byte[]{ buf.get(12), buf.get(13), buf.get(14), buf.get(15) });
+            destinationAddress = (Inet4Address) Inet4Address.getByAddress(new byte[]{ buf.get(16), buf.get(17), buf.get(18), buf.get(19) });
+
+        }catch(UnknownHostException e){
+            throw new IllegalArgumentException("Address isn't a valid host.");
         }
 
         protocol = IPProtocols.fromCode(buf.get(9));
--- src/net/ethernaught/packet/layers/ip/inter/IPVersions.java
+++ src/net/ethernaught/packet/layers/ip/inter/IPVersions.java
@@ -1,4 +1,41 @@
 package net.ethernaught.packet.layers.ip.inter;
 
 public enum IPVersions {
+
+    IPV4 {
+        @Override
+        public byte getCode(){
+            return 4;
+        }
+
+        @Override
+        public String toString(){
+            return "IPv4";
+        }
+    },
+    IPV6 {
+        @Override
+        public byte getCode(){
+            return 6;
+        }
+
+        @Override
+        public String toString(){
+            return "IPv6";
+        }
+    };
+
+    public static IPVersions fromCode(byte code){
+        for(IPVersions c : values()){
+            if(c.getCode() == code){
+                return c;
+            }
+        }
+
+        throw new IllegalArgumentException("No enum constant "+IPVersions.class.getName()+" "+code);
+    }
+
+    public byte getCode(){
+        return 0;
+    }
 }
